ce4c1e6e43948c8ea531d1c2eda94708513d9ae6
saifulriza123 <mail.saifulriza@gmail.com>
2021-06-18 20:02:03 +0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# GSP329
+
+## How to use
+
+run `bash run.sh`
---
+++ run.sh
@@ -0,0 +1,12 @@
+export PROJECT=$(gcloud config get-value project)
+export BUCKET_NAME=$PROJECT
+export PROJECT_NAME=$PROJECT
+# export DATASET_NAME=image_classification_dataset
+
+bash create_credential.sh
+
+gsutil cp gs://$BUCKET_NAME/analyze-images.py .
+
+cat script.py > analyze-images.py
+
+python3 analyze-images.py $PROJECT_NAME $BUCKET_NAME
